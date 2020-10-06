1f426d2e63b8adcaf8173898fd723b66d7e63aa4
Robin Gagnon <me@reobin.dev>
2020-10-05 20:48:17 -0400

    feat: Use http-proxy-middleware to proxy requests

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
-const express = require("express");
-const request = require("request");
-const cors = require("cors");
-
 require("dotenv").config();
 
-const elasticsearchHost =
-  process.env.ELASTICSEARCH_URL || "http://localhost:9200";
+const express = require("express");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const port = Number(process.env.ELASTICSEARCH_API_PORT || 3000);
+const proxy = createProxyMiddleware({
+  target: process.env.ELASTICSEARCH_HOST || "http://localhost:9200",
+  changeOrigin: true,
+});
 
 const app = express();
-
 app.use(cors());
-
-app.use("/", (req, res) => {
-  if (req.url !== "/favicon.ico") {
-    if (!(req.method == "GET" || req.method == "POST")) {
-      errMethod = {
-        error:
-          req.method + " request method is not supported. Use GET or POST.",
-      };
-      res.write(JSON.stringify(errMethod));
-      res.end();
-      return;
-    }
-
-    const url = `${elasticsearchHost}${req.url}`;
-    req
-      .pipe(
-        request({
-          uri: url,
-          auth: {
-            user: "username",
-            pass: "password",
-          },
-          headers: {
-            "accept-encoding": "none",
-          },
-          rejectUnauthorized: false,
-        }),
-      )
-      .pipe(res);
-  }
-});
-
-app.listen(port, () => {
-  console.log(`elasticsearch is running on ${elasticsearchHost}`);
-  console.log(
-    `vimcolorschemes-search API is running on http://localhost:${port}`,
-  );
-});
+app.use("/", proxy);
+app.listen(process.env.PORT || 3000);
