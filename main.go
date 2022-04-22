5ea25a43ab8a23e46a21439282f9b10f8240c2ae
Robin Gagnon <me@reobin.dev>
2022-04-22 18:49:20 -0500

    feat: Fetch and return arbitrary search index

--- main.go
+++ main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/vimcolorschemes/search/internal/dotenv"
+	"github.com/vimcolorschemes/search/internal/database"
 )
 
-type MyEvent struct {
+type Event struct {
 	Name string `json:"What is your name?"`
 	Age  int    `json:"How old are you?"`
 }
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
-
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	test, exists := dotenv.Get("SEARCH_TEST")
-
-	if exists {
-		return MyResponse{Message: fmt.Sprintf("%s is %d years old! %s", event.Name, event.Age, test)}, nil
-	}
-
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old! no dotenv", event.Name, event.Age)}, nil
+func HandleLambdaEvent(event Event) (interface{}, error) {
+	searchIndex := database.GetSearchIndex()
+	return searchIndex, nil
 }
 
 func main() {
