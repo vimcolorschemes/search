6bfb8af06a9076bd8b5e1610c06e9aa222a89872
Robin Gagnon <me@reobin.dev>
2022-04-22 17:08:08 -0500

    build: Add request payload to function invocation

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
-func handleRequest() (string, error) {
-	return "Hello from Go!", nil
+type MyEvent struct {
+	Name string `json:"What is your name?"`
+	Age  int    `json:"How old are you?"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
 func main() {
-	lambda.Start(handleRequest)
+	lambda.Start(HandleLambdaEvent)
 }
